f574497a17c68ac2385a25183155765266aea204
Atron91 <pruizmarin2@gmail.com>
2022-07-28 17:33:46 +0200

    Formularios Reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup ({ 
+  //   nombre: new FormControl('TITAN X'),
+  //   precio: new FormControl( 1500 ),
+  //   existencias: new FormControl( 5 )
+
+  // });
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ , [ Validators.required, Validators.minLength(3)] ],
+    precio: [ , [ Validators.min(0), Validators.required ]],
+    existencias: [ , [ Validators.min(0), Validators.required ]],
+  })
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'TITAN X',
+      precio: 1200,
+      existencias: 5
+    })
+    
+  }
+
+
+  campoEsValido(campo: string){
+    
+    return this.miFormulario.controls[campo].errors
+        && this.miFormulario.controls[campo].touched;
+  }
+  
+  guardar(){
+
+    if( this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset();
   }
 
 }
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
